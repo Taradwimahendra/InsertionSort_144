34b45353eb1804c6ebc09afa2bceaf4a2e6cd372
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-03-25 18:11:47 +0700

    input void insertionSort

--- file1.cpp
+++ file1.cpp
@@ -20,5 +20,34 @@ void input()
             cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
         }
     }
-    
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "===================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data Ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionSort()
+{
+
+    int temp; 
+    int j, i;
+
+    for (i = 1; i <= n - 1; i++)
+    {
+
+        temp = arr[i];
+
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+    }
 }
