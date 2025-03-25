232df05e7feb1f6ac15d007ae2ab0cdf9d1649db
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-03-25 18:20:21 +0700

    input main display and system "Pause"

--- file1.cpp
+++ file1.cpp
@@ -68,4 +68,14 @@ void display()
     {
         cout <<arr[j] << endl;
     }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    insertionSort();
+    display();
+    system("Pause");
+    return 0;
 }
